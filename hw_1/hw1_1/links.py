cc46cb71b5f3c11ac07e9824fe1b915a64e02962
Anthony <anthony@iMac-Anthony.local>
2020-10-21 23:05:51 +0300

    hw1_1 added state checks

--- hw_1/hw1_1/links.py
+++ hw_1/hw1_1/links.py
@@ -4,13 +4,16 @@ import argparse
 import asyncio
 
 
-# stores hash of file as key and path of file as value
+# stores hash of a file as key and path of file as value
 files_hash = {}
+# stores state checks of a file
+state_checks = {}
 
 
 # searching for duplicates and transform them into hard links
 async def hard_link(dirpath, dirnames, filenames):
     global files_hash
+    global state_checks
     for filename in filenames:
         file_path = os.path.join(dirpath, filename)
         stat = list(os.stat(file_path))
@@ -23,13 +26,16 @@ async def hard_link(dirpath, dirnames, filenames):
         if filehash in files_hash.keys():
             # check if it is already hard link
             if id == files_hash[filehash][1]:
+                state_checks[file_path] = 'is already a hard link'
                 continue
 
             originfile = files_hash[filehash][0]
             os.remove(file_path)
             os.link(originfile, file_path)
+            state_checks[file_path] = 'was a duplicate. now a hard link'
         else:
             files_hash[filehash] = [file_path, id]
+            state_checks[file_path] = 'is origin'
     return files_hash
 
 
