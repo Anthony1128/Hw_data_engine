a753879d0a9894c55a870d50623c074dc0ef61f7
Anthony <anthony@iMac-Anthony.local>
2020-11-16 02:01:25 +0300

    hw6_1 data generator fixed

--- hw_6/hw6_1/data_generator.py
+++ hw_6/hw6_1/data_generator.py
@@ -2,9 +2,25 @@ import pandas as pd
 import numpy as np
 import random
 
+YEAR = 2019
 
-# Generates tuple of random start and end dates
-def random_date():
+
+# Generates random Data Frame with n rows
+def data_generate(n):
+    data_array = []
+    for i in range(n):
+        start_date = start_date_generate(YEAR)
+        end_date = end_date_generate(YEAR, start_date)
+        data_array.append([i, start_date, end_date])
+    np_array = np.array(data_array)
+    df = pd.DataFrame(data=np_array,
+                      columns=['Name', 'Start Date',
+                               'End Date']).set_index('Name')
+    return df
+
+
+# Generates random start date
+def start_date_generate(year):
     rd_month = random.randint(1, 12)
     if rd_month in [1, 3, 5, 7, 8, 10, 12]:
         rd_day = random.randint(1, 31)
@@ -14,29 +30,23 @@ def random_date():
         rd_day = random.randint(1, 30)
     rd_hour = random.randint(0, 23)
     rd_minute = random.randint(0, 59)
-    start_date = pd.Timestamp(2019, rd_month, rd_day, rd_hour, rd_minute)
-    time_delta = pd.Timestamp(2020, 1, 1) - start_date
-    delta = start_date.value + random.randint(0,
-                                              time_delta.value/10**10)*10**10
-    end_date = pd.Timestamp(delta).round(freq='T')
-    # return start_date.isoformat(), end_date.isoformat()
-    return start_date, end_date
+    start_date = pd.Timestamp(year, rd_month, rd_day, rd_hour, rd_minute)
+    return start_date
 
 
-# Generates random Data Frame with n rows
-def data_generate(n):
-    data_array = []
-    for i in range(n):
-        dates = random_date()
-        data_array.append([i, dates[0], dates[1]])
-    np_array = np.array(data_array)
-    df = pd.DataFrame(data=np_array,
-                      columns=['Name', 'Start Date',
-                               'End Date']).set_index('Name')
-    return df
-
-
-
+# Generates random end date
+def end_date_generate(year, start_date):
+    rd_month = random.randint(start_date.month, 12)
+    if rd_month in [1, 3, 5, 7, 8, 10, 12]:
+        rd_day = random.randint(1, 31)
+    elif rd_month == 2:
+        rd_day = random.randint(1, 28)
+    else:
+        rd_day = random.randint(1, 30)
+    rd_hour = random.randint(0, 23)
+    rd_minute = random.randint(0, 59)
+    end_date = pd.Timestamp(year, rd_month, rd_day, rd_hour, rd_minute)
+    return end_date
 
 
 
