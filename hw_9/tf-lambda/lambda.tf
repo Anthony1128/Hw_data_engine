f90d307f42a8d33e7faaf9d4ef00dc87a834f84e
Anthony <change_me@later.com>
2021-02-02 01:58:40 +0300

    hw_9 tf triggers added

--- hw_9/tf-lambda/lambda.tf
+++ hw_9/tf-lambda/lambda.tf
@@ -12,6 +12,7 @@ provider "aws" {
   profile = "default"
 }
 
+// ----- IAM settings -----
 resource "aws_iam_policy" "lambda_policy_s3" {
   name        = "lambda_policy_s3"
   path        = "/"
@@ -56,6 +57,15 @@ resource "aws_iam_role_policy_attachment" "lambda_iam_attach" {
   role = aws_iam_role.iam_for_lambda.name
 }
 
+resource "aws_lambda_permission" "allow_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.aws-de-lambda-func-tf.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = data.aws_s3_bucket.bucket.arn
+}
+
+// ----- Deploying lambda function -----
 locals {
   my_function_source = "zip_package/aws_package_lambda_tf.zip"
 }
@@ -86,4 +96,42 @@ resource "aws_lambda_function" "aws-de-lambda-func-tf" {
   source_code_hash = data.archive_file.lambda_func.output_base64sha256
   depends_on = [aws_s3_bucket_object.lambda_zip_s3]
   runtime = "python3.8"
+}
+
+// ----- Adding CloudWatch trigger -----
+resource "aws_cloudwatch_event_rule" "every_day_at_time" {
+  name = "every_day_at_time"
+  description = "every_day_at_time"
+  schedule_expression = "cron(00 13 ? * MON-FRI *)"
+}
+
+resource "aws_cloudwatch_event_target" "check_func" {
+  rule = aws_cloudwatch_event_rule.every_day_at_time.name
+  target_id = "aws-de-lambda-func-tf"
+  arn = aws_lambda_function.aws-de-lambda-func-tf.arn
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_func" {
+  statement_id = "AllowExecutionFromCloudWatch"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.aws-de-lambda-func-tf.function_name
+  principal = "events.amazonaws.com"
+  source_arn = aws_cloudwatch_event_rule.every_day_at_time.arn
+}
+
+// ----- Adding S3 trigger -----
+data "aws_s3_bucket" "bucket" {
+  bucket = "de-source-csv-for-lambda"
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = data.aws_s3_bucket.bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.aws-de-lambda-func-tf.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_suffix       = ".csv"
+  }
+
+  depends_on = [aws_lambda_permission.allow_bucket]
 }
